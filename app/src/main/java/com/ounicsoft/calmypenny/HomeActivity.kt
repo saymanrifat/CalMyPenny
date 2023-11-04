517c0e723f691ab081dd66974fc070f56c5327e7
Sayman Rifat <98202630+saymanrifat@users.noreply.github.com>
2023-11-04 15:53:19 +0600

    HomeScreen Navigation and fragment add with working condition

--- app/src/main/java/com/ounicsoft/calmypenny/HomeActivity.kt
+++ app/src/main/java/com/ounicsoft/calmypenny/HomeActivity.kt
@@ -2,10 +2,39 @@ package com.ounicsoft.calmypenny
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.ounicsoft.calmypenny.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val overViewFragment = OverViewFragment()
+        val budgetFragment = BudgetFragment()
+        val walletFragment = WalletFragment()
+
+        setCurrentFragment(overViewFragment)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_overView -> setCurrentFragment(overViewFragment)
+                R.id.menu_budget -> setCurrentFragment(budgetFragment)
+                R.id.menu_wallet -> setCurrentFragment(walletFragment)
+
+            }
+            true
+        }
+
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.home_HostFragment, fragment)
+            commit()
+        }
+
 }
