ec0c53ef7d51fb157063dec4e9dcafef7a749f60
Sayman Rifat <98202630+saymanrifat@users.noreply.github.com>
2023-11-04 19:37:20 +0600

    remove toolbar

--- app/src/main/java/com/ounicsoft/calmypenny/view/home/OverViewFragment.kt
+++ app/src/main/java/com/ounicsoft/calmypenny/view/home/OverViewFragment.kt
@@ -5,15 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.ounicsoft.calmypenny.R
+import com.ounicsoft.calmypenny.databinding.FragmentOverViewBinding
 
 class OverViewFragment : Fragment() {
+    private lateinit var binding: FragmentOverViewBinding
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_over_view, container, false)
+    ): View {
+        binding = FragmentOverViewBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
     }
 
 }
