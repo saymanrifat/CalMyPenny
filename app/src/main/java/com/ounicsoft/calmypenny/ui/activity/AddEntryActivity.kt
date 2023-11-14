cedd4f4c4c42e6f78ea7085bcabaff112eb1e78f
Sayman Rifat <saymanrifat@gmail.com>
2023-11-14 22:59:45 +0600

    Work on Add Transaction Logics

--- app/src/main/java/com/ounicsoft/calmypenny/ui/activity/AddEntryActivity.kt
+++ app/src/main/java/com/ounicsoft/calmypenny/ui/activity/AddEntryActivity.kt
@@ -2,13 +2,34 @@ package com.ounicsoft.calmypenny.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.ounicsoft.calmypenny.R
 import com.ounicsoft.calmypenny.databinding.ActivityAddEntryBinding
 
 class AddEntryActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddEntryBinding
+    private lateinit var walletSpinner: Spinner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddEntryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //Initialization Views
+        walletSpinner = binding.spSelectWalletFrom
+        //Spinner Setup
+        setupSpinnerForExpense()
+
+    }
+
+    private fun setupSpinnerForExpense() {
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.sample_wallet_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            walletSpinner.adapter = adapter
+        }
     }
 }
